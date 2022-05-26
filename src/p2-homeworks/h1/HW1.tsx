dabf11019a68dc00a0a5f39e484054d504896085
Evgenii <emeleshkov@mail.ru>
2022-05-26 04:47:23 +0300

    Message.tsx & Message.module.css modified

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,26 +1,30 @@
 import React from 'react'
+import Message from "./Message";
 
-const messageData = {
+export const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
+    name: 'Neco Chan',
+    message: 'Ohiyo! Oni chan! ^_^',
     time: '22:00',
 }
 
 function HW1() {
+
     return (
         <div>
             <hr/>
             homeworks 1
-
+            <hr/>
+            Kaway
+            <hr/>
             {/*should work (должно работать)*/}
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import styles from "./Message.module.css"
+import {messageData} from "./HW1";
 
-function Message() {
+type messageDataType = typeof messageData
+
+const Message = ({avatar, name, message, time}: messageDataType) => {
+
     return (
         <div>
-
+            <div className={styles.messageInstance}>
+                <div>
+                    <img className={styles.img} src={avatar}/>
+                </div>
+                <div>
+                    <div className={styles.messageText}>
+                        <div className={styles.userName}>{name}</div>
+                        <div className={styles.userMessage}>
+                            {message}
+                        </div>
+                        <div className={styles.userMessageTime}>
+                            {time}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/*<div>*/}
+            {/*    <textarea></textarea>*/}
+            {/*    <button>send</button>*/}
+            {/*</div>*/}
         </div>
     )
 }
