5791806d2a329e668591b57edff1bfa2d07375e8
Evgenii <emeleshkov@mail.ru>
2022-06-07 02:41:27 +0300

    third task done

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,32 @@
-import React from 'react'
+import React, {ChangeEvent, KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void // need to fix any
+    addUser: () => void // need to fix any
+    error: string // need to fix any
+    totalUsers: number // need to fix any
+    onEnterPressUpCallBack: (e: KeyboardEvent<HTMLInputElement>)=>void
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+    {name, setNameCallback, addUser, error, totalUsers, onEnterPressUpCallBack} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
-
+    const inputClass = error !== '' ? s.error : s.someClass// need to fix with (?:)
+    const buttonClass = error !== '' ? s.errorButton : s.normButton
     return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+        <div className={s.mainDiv}>
+            <input onKeyUp={onEnterPressUpCallBack} value={name} onChange={setNameCallback} className={inputClass}/>
+
+            {error === '' && name.match(/\w/)
+                ? <button className={buttonClass} onClick={addUser}>add</button>
+                : <button className={buttonClass} disabled={true}>Enter valid name</button>
+            }
+            <span className={s.totalUsersSpan}>{totalUsers}</span>
+            <div><span className={s.errorText}>{error}</span></div>
+
         </div>
     )
 }
--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,9 +1,11 @@
-import React, {useState} from 'react'
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
 import Greeting from './Greeting'
+import {UserType} from './HW3';
+import {v1} from 'uuid';
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: Array<UserType> // need to fix any
+    addUserCallback: (name: string) => void // need to fix any
 }
 
 // более простой и понятный для новичков
@@ -12,26 +14,45 @@ type GreetingContainerPropsType = {
 // более современный и удобный для про :)
 // уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    const [name, setName] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string>('') // need to fix any
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+        let eventValue = e.currentTarget.value
+        setName(eventValue)
+        if (eventValue.match(/(^\D\w\D+)$/gi)) {
+            setName(eventValue)
+            setError('')
+        } else if (eventValue === '') {
+            setError('Enter your name')
+        } else if (eventValue.match(/\d+/gi)) {
+            setError('Print only letters')
+        } else if (eventValue.length < 3 || eventValue.match(/\d+/gi)) {
+            setError('Name should be at list 3 letters long')
+        }
+    }
+    const onEnterPressUpCallBack = (e: KeyboardEvent<HTMLInputElement>) => {
+        e.key === 'Enter' && error === '' && addUser()
     }
     const addUser = () => {
-        alert(`Hello  !`) // need to fix
+        addUserCallback(name)
+        alert(`Hello ${name} !`)// need to fix
+        setName('')
     }
 
-    const totalUsers = 0 // need to fix
-
+    let totalUsers = users.length // need to fix
     return (
-        <Greeting
-            name={name}
-            setNameCallback={setNameCallback}
-            addUser={addUser}
-            error={error}
-            totalUsers={totalUsers}
-        />
+        <>
+            <Greeting
+                onEnterPressUpCallBack={onEnterPressUpCallBack}
+                name={name}
+                setNameCallback={setNameCallback}
+                addUser={addUser}
+                error={error}
+                totalUsers={totalUsers}
+            />
+            <ul>{users.map(el => <li key={v1()}>{el.name}</li>)}</ul>
+        </>
     )
 }
 
