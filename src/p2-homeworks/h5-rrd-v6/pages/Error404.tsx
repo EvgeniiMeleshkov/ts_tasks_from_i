7e44aa71981baa8f8e5e14618305403923a68226
Evgenii <emeleshkov@mail.ru>
2022-06-19 01:16:26 +0300

    5-th done

--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import s from './Error404.module.css'
+import Keanu from '../../../assets/Keanu.png'
+import leo from '../../../assets/leo.png'
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={s.main}>
+            <div className={s.textError}>
+                <div>404</div>
+                <div>Page not found! 🙂</div>
+            </div>
+            <span>
+                <img className={s.leo} src={leo}/>
+                <img className={s.keanu} src={Keanu}/>
+            </span>
         </div>
     )
 }
