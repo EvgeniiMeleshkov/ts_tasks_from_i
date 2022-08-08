e250e3c5646857d52cacca5b153a3d5ad6eed063
Evgenii <emeleshkov@mail.ru>
2022-08-08 23:29:31 +0300

    11 refactored

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -3,23 +3,13 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
-
     const onTopSliderChanged = (value: number) => {
-        if (value <= value2) {
-            setValue1(value)
-            setDoubleVal([value, value2])
+        if (value < doubleVal[1]) {
+            setDoubleVal([value, doubleVal[1]])
         }
-
     }
 
     const setDoubleValue = (value: [number, number]) => {
-        if (value1 >= value2) {
-            return
-        }
-        setValue1(value[0])
-        setValue2(value[1])
         setDoubleVal(value)
     }
     const [doubleVal, setDoubleVal] = useState<[number, number]>([0, 100])
@@ -32,19 +22,19 @@ function HW11() {
             {/*should work (должно работать)*/}
             <div style={{display: 'flex',flexDirection: 'column', alignItems: 'center'}}>
                 <div>
-                    <span>{value1}</span>
-                    <SuperRange value={value1}
+                    <span>{doubleVal[0]}</span>
+                    <SuperRange value={doubleVal[0]}
                                 onChangeRange={onTopSliderChanged}
 
                     />
                 </div>
 
                 <div>
-                    <span>{value1}</span>
+                    <span>{doubleVal[0]}</span>
                     <SuperDoubleRange value={doubleVal} onChangeRange={setDoubleValue}
 
                     />
-                    <span>{value2}</span>
+                    <span>{doubleVal[1]}</span>
                 </div>
             </div>
             <hr/>
