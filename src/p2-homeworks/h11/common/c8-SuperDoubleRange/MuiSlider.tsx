1a432488f2f23adfb78c174f40963a65b99f4644
Evgenii <emeleshkov@mail.ru>
2022-09-19 16:05:10 +0300

    ...

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/MuiSlider.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/MuiSlider.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 import '../OblivionLoadingStyle'
 import {oblivionLoadingStyle} from '../OblivionLoadingStyle';
